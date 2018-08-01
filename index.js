c4d4d34a3862f0acd506f718ab6671207d674be0
dyuki <unblivion@gmail.com>
2018-07-31 23:53:42 -0300

    adicionando novo botão + imput para filtro

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-let botao = document.querySelector("button");
+let botao = document.querySelector("#rodar");
 let lista = document.querySelector("div");
+let filtro = document.querySelector("#filtro");
+let vaivem = true;
+
+filtro.addEventListener("click", () => {
+    modalVaiVem();
+})
+
+function modalVaiVem() {
+    if (vaivem == true) {
+        document.querySelector("#valorFiltro").style.transform = "scaleX(1)";
+        document.querySelector("#filtro").style.borderRadius = "0 100px 100px 0";
+        vaivem = false;
+    }
+    else {
+        document.querySelector("#valorFiltro").style.transform = " translateX(123px) scaleX(0.01)";
+        document.querySelector("#filtro").style.borderRadius = "100px";
+        vaivem = true;
+    }
+}
 
 botao.addEventListener("click", () => {
     fetch("https://api.chucknorris.io/jokes/random").then(resposta => {
